671630c8ca691392789a25b56b70968439a3f837
Arturo <arturocs@protonmail.com>
2023-11-21 23:43:23 +0100

    Remove Shared2 based on qcell

--- src/shared.rs
+++ src/shared.rs
@@ -1,49 +1,30 @@
-use std::rc::Rc;
 use std::cell::RefCell;
-
-struct Shared<T> {
+use std::rc::Rc;
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Shared<T: Clone> {
     value: Rc<RefCell<T>>,
 }
 
-impl<T> Shared<T> {
-    fn new(value: T) -> Self {
+impl<T: Clone> Shared<T> {
+    pub fn new(value: T) -> Self {
         Shared {
             value: Rc::new(RefCell::new(value)),
         }
     }
 
-    fn borrow(&self) -> std::cell::Ref<T> {
+    pub fn borrow(&self) -> std::cell::Ref<T> {
         self.value.borrow()
     }
 
-    fn borrow_mut(&self) -> std::cell::RefMut<T> {
+    pub fn borrow_mut(&self) -> std::cell::RefMut<T> {
         self.value.borrow_mut()
     }
-}
 
-use qcell::{TCell, TCellOwner};
-
-pub struct Marker;
-pub type ACell<T> = TCell<Marker, T>;
-pub type ACellOwner = TCellOwner<Marker>;
-#[derive(Clone)]
-pub struct Shared2<T>(Rc<ACell<T>>);
-
-impl<T> Shared2<T> {
-    fn borrow<'a>(&'a self, owner: &'a ACellOwner) -> &'a T {
-        owner.ro(&self.0)
+    pub fn as_ptr(&self) -> *const T {
+        self.value.as_ptr()
     }
 
-    fn borrow_mut<'a>(&'a self, owner: &'a mut ACellOwner) -> &'a T {
-        owner.rw(&self.0)
-    }
-
-    fn new(value: T) -> Self {
-        Shared2(Rc::new(ACell::new(value)))
-    }
-
-    fn into_inner(self) -> T {
-        Rc::try_unwrap(self.0).ok().unwrap().into_inner()
+    pub fn unwrap_or_clone(self) -> RefCell<T> {
+        Rc::try_unwrap(self.value).unwrap_or_else(|rc| (*rc).clone())
     }
 }
-
