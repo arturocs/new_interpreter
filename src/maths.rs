778327a7c411fc7910f73657d646bedfdbe69511
Arturo <arturocs@protonmail.com>
2023-11-24 23:54:30 +0100

    Make math library accesible from the interpreter

--- src/maths.rs
+++ src/maths.rs
@@ -1,19 +1,22 @@
-use crate::variant::{Float, Variant};
+use crate::{
+    memory::Memory,
+    variant::{Float, Variant},
+};
 use anyhow::{bail, Result};
 use std::f64;
 
 macro_rules! impl_unary_math_functions {
     ($($func:ident),+) => {
         impl Variant {
-            fn unary_math_function_wrapper(self, function: fn(f64) -> f64, name: &str) -> Result<Self>{
+            fn unary_math_function_wrapper(&self, function: fn(f64) -> f64, name: &str) -> Result<Self>{
                 match self {
-                    Variant::Float(i) => Ok(Variant::Float(function(i))),
-                    Variant::Int(i) => Ok(Variant::Float(function(i as Float))),
+                    &Variant::Float(i) => Ok(Variant::Float(function(i))),
+                    &Variant::Int(i) => Ok(Variant::Float(function(i as Float))),
                     _ => bail!("Cannot calculate {name} of {self:?}")
                 }
             }
             $(
-                pub fn $func(self) -> Result<Self> {
+                pub fn $func(&self) -> Result<Self> {
                     self.unary_math_function_wrapper(f64::$func, stringify!($func))
                 }
             )+
@@ -27,12 +30,12 @@ impl Variant {
     pub const PI: Variant = Variant::Float(f64::consts::PI);
     pub const E: Variant = Variant::Float(f64::consts::E);
 
-    pub fn log(self, base: Variant) -> Result<Self> {
+    pub fn log(&self, base: &Variant) -> Result<Self> {
         let result = match (self, base) {
-            (Variant::Int(a), Variant::Int(b)) => Variant::Float((a as Float).log(b as Float)),
-            (Variant::Float(a), Variant::Float(b)) => Variant::Float(a.log(b)),
-            (Variant::Int(a), Variant::Float(b)) => Variant::Float((a as Float).log(b)),
-            (Variant::Float(a), Variant::Int(b)) => Variant::Float(a.log(b as Float)),
+            (&Variant::Int(a), &Variant::Int(b)) => Variant::Float((a as Float).log(b as Float)),
+            (&Variant::Float(a), &Variant::Float(b)) => Variant::Float(a.log(b)),
+            (&Variant::Int(a), &Variant::Float(b)) => Variant::Float((a as Float).log(b)),
+            (&Variant::Float(a), &Variant::Int(b)) => Variant::Float(a.log(b as Float)),
             (value, base) => {
                 bail!("Cannot calculate log of {value:?} with base {base:?}")
             }
@@ -50,6 +53,62 @@ impl Variant {
         };
         Ok(result)
     }
+}
+
+fn wrap_unary_math_function(
+    f: fn(&Variant) -> Result<Variant>,
+) -> impl Fn(&[Variant], &mut Memory) -> Variant {
+    move |args: &[Variant], _: &mut Memory| {
+        if args.len() != 1 {
+            Variant::error(format!("Expected 1 argument, got {}", args.len()));
+        }
+        f(&args[0]).unwrap_or_else(|e| Variant::error(e))
+    }
+}
+
+fn wrap_binary_math_function(
+    f: fn(&Variant, &Variant) -> Result<Variant>,
+) -> impl Fn(&[Variant], &mut Memory) -> Variant {
+    move |args: &[Variant], _: &mut Memory| {
+        if args.len() != 2 {
+            Variant::error(format!("Expected 2 arguments, got {}", args.len()));
+        }
+        f(&args[0], &args[1]).unwrap_or_else(|e| Variant::error(e))
+    }
+}
+
+pub fn export_math_lib() -> Variant {
+    let unary_functions = [
+        ("abs", Variant::abs as fn(&Variant) -> Result<Variant>),
+        ("acos", Variant::acos),
+        ("asin", Variant::asin),
+        ("atan", Variant::atan),
+        ("ceil", Variant::ceil),
+        ("cos", Variant::cos),
+        ("exp", Variant::exp),
+        ("floor", Variant::floor),
+        ("ln", Variant::ln),
+        ("round", Variant::round),
+        ("sin", Variant::sin),
+        ("sqrt", Variant::sqrt),
+        ("tan", Variant::tan),
+    ]
+    .map(|(name, f)| (name, Variant::native_fn(wrap_unary_math_function(f))));
+
+    let log = Variant::log as fn(&Variant, &Variant) -> Result<Variant>;
+    let binary_functions = [("log", log), ("pow", Variant::pow)]
+        .map(|(name, f)| (name, Variant::native_fn(wrap_binary_math_function(f))));
+    
+    let constants = [("PI", Variant::PI), ("E", Variant::E)];
+
+    Variant::dict(
+        &unary_functions
+            .into_iter()
+            .chain(binary_functions)
+            .chain(constants)
+            .map(|(name, f)| (Variant::str(name), f))
+            .collect::<Vec<_>>(),
+    )
 }
 
 #[cfg(test)]
--- src/memory.rs
+++ src/memory.rs
@@ -1,4 +1,8 @@
-use crate::{builtins, variant::{Variant, Type}};
+use crate::{
+    builtins,
+    maths::export_math_lib,
+    variant::{Type, Variant},
+};
 use ahash::AHashMap;
 use anyhow::{anyhow, bail, Context, Ok, Result};
 use std::{rc::Rc, vec};
@@ -16,7 +20,7 @@ impl Memory {
 
     pub fn with_builtins() -> Self {
         let sum: fn(&[Variant], &mut Memory) -> Variant = builtins::sum;
-        let context = vec![
+        let context = [
             ("sum", sum, None),
             ("prod", builtins::prod, None),
             ("min", builtins::min, None),
@@ -27,11 +31,27 @@ impl Memory {
             ("input", builtins::input, None),
             ("push", builtins::push, Some(vec![Type::Vec])),
             ("range", builtins::range, None),
-            ("contains", builtins::contains, Some(vec![Type::Vec, Type::Dict])),
-            ("join", builtins::join, Some(vec![Type::Vec, Type::Iterator])),
+            (
+                "contains",
+                builtins::contains,
+                Some(vec![Type::Vec, Type::Dict]),
+            ),
+            (
+                "join",
+                builtins::join,
+                Some(vec![Type::Vec, Type::Iterator]),
+            ),
             ("map", builtins::map, Some(vec![Type::Vec, Type::Iterator])),
-            ("filter", builtins::filter, Some(vec![Type::Vec, Type::Iterator])),
-            ("to_vec", builtins::to_vec, Some(vec![Type::Vec, Type::Iterator])),
+            (
+                "filter",
+                builtins::filter,
+                Some(vec![Type::Vec, Type::Iterator]),
+            ),
+            (
+                "to_vec",
+                builtins::to_vec,
+                Some(vec![Type::Vec, Type::Iterator]),
+            ),
             ("slice", builtins::slice, Some(vec![Type::Vec, Type::Str])),
         ]
         .into_iter()
@@ -42,6 +62,7 @@ impl Memory {
                 (name.into(), Variant::native_fn(f))
             }
         })
+        .chain(std::iter::once(("Math".into(), export_math_lib())))
         .collect();
 
         Memory(vec![context])
