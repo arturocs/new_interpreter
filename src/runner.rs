29643424328ba3ee430928c908abd4e133dd7caa
Arturo <arturocs@protonmail.com>
2023-12-30 16:02:47 +0100

    Improve bench runner

--- src/runner.rs
+++ src/runner.rs
@@ -2,6 +2,7 @@ use crate::{function::Function, memory::Memory, parser::expr_parser, variant::Va
 use anyhow::{bail, Result};
 use colored::Colorize;
 use itertools::Itertools;
+use std::io::{self, Write};
 use std::time::{Duration, Instant};
 use std::{fs, rc::Rc};
 
@@ -41,31 +42,42 @@ fn run_bench(
     name: &str,
     func: &Function,
     memory: &mut Memory,
-    warmup_seconds: u64,
-    bench_seconds: u64,
+    warmup_seconds: f64,
+    bench_seconds: f64,
 ) -> Result<()> {
-    println!("Running {name}...");
+    print!("Running {name}: Warming up for {warmup_seconds:.4}s ...");
+    io::stdout().flush().unwrap();
 
-    let warmup_time = Duration::from_secs(warmup_seconds);
+    let warmup_time = Duration::from_secs_f64(warmup_seconds);
     let mut iterations = 0;
     let start = Instant::now();
     while warmup_time > start.elapsed() {
         func.call(&[], memory)?;
         iterations += 1;
     }
-
     let average_time = warmup_time.as_secs_f64() / iterations as f64;
-    let bench_iterations = (bench_seconds as f64 / average_time).trunc() as u32;
-    let mut times = Vec::with_capacity(bench_iterations as usize);
+    let bench_iterations = (bench_seconds / average_time).round() as u32;
+    let expected_duration = Duration::from_secs_f64(bench_iterations as f64 * average_time);
+
+    let running_message = format!("\rRunning {name}: Collecting {bench_iterations} samples. Estimated time {expected_duration:.4?}");
+    print!("{running_message}");
+    io::stdout().flush().unwrap();
+
+    let mut times = Duration::from_secs(0);
     for _ in 0..bench_iterations {
         let t = Instant::now();
         func.call(&[], memory)?;
-        times.push(t.elapsed());
+        times += t.elapsed();
     }
-    let average_time = times.iter().sum::<Duration>() / bench_iterations as u32;
+
+    let average_time = times / bench_iterations;
     let time_formated = format!("{average_time:?}").bold();
     let colored_name = name.green().bold();
-    println!("Ran {colored_name} {bench_iterations} times. Average time: {time_formated}\n");
+    let padding = " ".repeat(running_message.len());
+
+    println!(
+        "\rRan {colored_name} {bench_iterations} times. Average time: {time_formated}{padding}"
+    );
     Ok(())
 }
 
@@ -79,7 +91,7 @@ pub fn run_benches_in_file(path: &str) -> Result<()> {
     }
     println!("\nFound {n_benches} benches in {path}:\n");
     for (bench_name, bench_function) in benches {
-        run_bench(&bench_name, &bench_function, &mut memory, 3, 5)?;
+        run_bench(&bench_name, &bench_function, &mut memory, 3., 5.)?;
     }
     Ok(())
 }
@@ -97,7 +109,7 @@ pub fn run_tests_in_file(path: &str) -> Result<()> {
     let failed_msg = "Failed!".bold().red();
     let ok_msg = "Ok!".bold().green();
     for (test_name, test_function) in tests {
-        print!("Running {}... ",test_name.bold());
+        print!("Running {}... ", test_name.bold());
         match test_function.call(&[], &mut memory) {
             Ok(Variant::Error(e)) => println!("{failed_msg}: {e}"),
             Err(e) => println!("{failed_msg}: {e}"),
